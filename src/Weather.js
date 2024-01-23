fdece70b1f9302f059e55f1c60ccea42a911d1b8
Harpreet Kahlon <hkahlon@winnipeg.ca>
2024-01-23 11:31:39 -0600

    Fix Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,23 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 import intl from 'react-intl-universal';
-
-const WeatherApp = ({ locale }) => {
+const WeatherApp = ({locale}) => {
   const [greeting, setGreeting] = useState('');
-  const [btnGetWeather, setBtnGetWeather] = useState('');
-  const [txtEnterCity, setTxtEnterCity] = useState('');
-
+  const [btnGetWeather, setbtnGetWeather] = useState('');
+  const [txtEnterCity, settxtEnterCity] = useState('');
+  
   const [city, setCity] = useState('');
   const [weather, setWeather] = useState(null);
 
   const apiKey = 'c03664bedb1f9d07ca52d9ed9bbe60e3';
 
-  useEffect(() => {
-    // Set initial values for greeting, button text, and placeholder
+
+  useEffect(() => { 
+    // Set initial values for greeting and buttontext
     setGreeting(intl.get('greeting'));
-    setBtnGetWeather(intl.get('btnGetWeather'));
-    setTxtEnterCity(intl.get('txtEnterCity'));
+    setbtnGetWeather(intl.get('btnGetWeather'));
+    settxtEnterCity(intl.get('txtEnterCity'));
   }, [locale]);
+
 
   const getWeatherData = async () => {
     try {
@@ -43,7 +44,28 @@ const WeatherApp = ({ locale }) => {
 
       {weather && (
         <div>
-          {/* Your weather details rendering code here */}
+          <h2>{weather.name}, {weather.sys.country}</h2>
+        
+          <p> <div>
+          <h5><img id="wicon" src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}  alt="Weather Icon" /></h5>
+            
+          </div>
+          <div>
+          {intl.get('description')}: {weather.weather[0].description}
+          </div>
+            </p>
+            <p>{intl.get('temperature')}: {Math.round(weather.main.temp)} °C</p>
+            <p>{intl.get('feels_like')}: {Math.round(weather.main.feels_like)} °C</p>
+            <p>{intl.get('temp_min')}: {Math.round(weather.main.temp_min)} °C</p>
+            <p>{intl.get('temp_max')}: {Math.round(weather.main.temp_max)} °C</p>
+            <p>{intl.get('pressure')}: {Math.round(weather.main.pressure)} Pa</p>
+
+            <p>{intl.get('visibility')}: {Math.round(weather.visibility)} m</p>
+            <p>{intl.get('wind')}:</p>
+            <p>{intl.get('speed')}: {Math.round(weather.wind.speed)} km/s</p>
+            <p>{intl.get('deg')}: {Math.round(weather.wind.deg)} °NW</p>
+          
+
         </div>
       )}
     </div>
